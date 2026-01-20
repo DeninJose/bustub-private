85e5a026486580f1db2ef6c70951b5dcbb568696
Denin Jose <deninjose8@gmail.com>
2026-01-20 16:22:46 +0530

    Implemented the disk scheduler as well

--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -13,12 +13,13 @@
 #include "storage/disk/disk_scheduler.h"
 #include <vector>
 #include "common/macros.h"
+#include "common/exception.h"
 #include "storage/disk/disk_manager.h"
 
 namespace bustub {
 
 DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
-  UNIMPLEMENTED("TODO(P1): Add implementation.");
+  // UNIMPLEMENTED("TODO(P1): Add implementation.");
   // Spawn the background thread
   background_thread_.emplace([&] { StartWorkerThread(); });
 }
@@ -38,7 +39,12 @@ DiskScheduler::~DiskScheduler() {
  *
  * @param requests The requests to be scheduled.
  */
-void DiskScheduler::Schedule(std::vector<DiskRequest> &requests) {}
+void DiskScheduler::Schedule(std::vector<DiskRequest> &requests) {
+  // Add all the requests into the channel
+  std::for_each(requests.begin(), requests.end(), [this](auto& request){
+    request_queue_.Put(std::make_optional<DiskRequest>(std::move(request)));
+  });
+}
 
 /**
  * TODO(P1): Add implementation
@@ -48,6 +54,28 @@ void DiskScheduler::Schedule(std::vector<DiskRequest> &requests) {}
  * The background thread needs to process requests while the DiskScheduler exists, i.e., this function should not
  * return until ~DiskScheduler() is called. At that point you need to make sure that the function does return.
  */
-void DiskScheduler::StartWorkerThread() {}
+void DiskScheduler::StartWorkerThread() {
+  /**
+   * while there are requests in the queue,
+   * keep processing them
+   * If a null optional is found,
+   * break out from the processing loop
+   */
+  std::optional<DiskRequest> diskRequest = request_queue_.Get();
+
+  while (diskRequest.has_value()) {
+    auto& request = diskRequest.value();
+
+    if (request.is_write_) {
+      disk_manager_->WritePage(request.page_id_, request.data_);
+    } else {
+      disk_manager_->ReadPage(request.page_id_, request.data_);
+    }
+    request.callback_.set_value(true);
+
+    // Get next request
+    diskRequest = request_queue_.Get();
+  }
+}
 
 }  // namespace bustub
